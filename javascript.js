40629bfee7677801374558c8e1e704c100990e5c
KingZCoding <traenknercody@gmail.com>
2024-08-18 18:59:41 -0400

    finishing the lesson

--- javascript.js
+++ javascript.js
@@ -95,4 +95,95 @@ const web = "W3Schools";
 let letter = web.at(2); //let letter = web.at[2]; also works
 console.log(letter)
 
+//slicing strings
+//slice(start, end)
+//substring(start, end)
+//substr(start, length)
+// if first number - counts from end of string
+
+let text1 = "Apple, Banana, Kiwi";
+let part = text1.slice(-12, -6);
+console.log(part);
+
+let str = "Apple, Banana, Kiwi";
+let part2 = str.substring(7, 13);
+console.log(part2);
+
+let str2 = "Apple, Banana, Kiwi";
+let part3 = str2.substr(7, 6); //can ommit second param to slice rest fo string
+console.log(part3);
+
+//converting text to Upper Case
+let text3 = "Hello World!";
+let text4 = text3.toUpperCase();
+console.log(text4);
+
+//converting to Lower Case
+let text5 = "Hello World!";
+let text6 = text5.toLowerCase();
+console.log(text6);
+
+//concat() joins two or more strings
+let text7 = "Hello";
+let text8 = "World";
+let text9 = text7.concat(" ", text8);
+console.log(text9);
+
+//string trim
+//trim() removes white space from both sides
+//trimStart() removes from start
+//trimEnd() removes from end
+/*
+let text1 = "   Hello World!    ";
+let text2 = text1.trim();
+
+let text1 = "   Hello World!    ";
+let text2 = text1.trimStart();
+
+let text1 = "   Hello World!    ";
+let text2 = text1.trimEnd();
+
+*/
+/*
+//padStart
+let num = "5";
+let padded = num.padStart(4,"0");
+console.log(padded);
+
+//padEnd
+let num = "5";
+let padded = num.padEnd(4,"0");
+console.log(padded);
+*/
+//to pad a number turn it into a string first
+let numb = 5;
+let text10 = numb.toString();
+let padded = text10.padEnd(4,"0");
+console.log(padded);
+
+//repeat a string
+let text11 = "Hello world!";
+let result = text11.repeat(2);
+console.log(result);
+
+//replace something in a string
+let text12 = "Please visit Microsoft!";
+let newText = text12.replace("Microsoft", "W3Schools");
+console.log(newText);
+
+// using /i makes case not matter
+//let text = "Please visit Microsoft!";
+//let newText = text.replace(/MICROSOFT/i, "W3Schools");
+
+// using /g will replace all in the file
+//let text = "Please visit Microsoft and Microsoft!";
+//let newText = text.replace(/Microsoft/g, "W3Schools");
+
+
+
+
+
+
+
+
 
